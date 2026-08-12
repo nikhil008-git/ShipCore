5164202eb10215a20069899861a2406b6056e43a
nikhil rajpurohit <gittthub008@gmail.com>
2026-08-12 14:14:02 +0530

    ef core client done

--- Shipping.Api/Data/AppDbContext.cs
+++ Shipping.Api/Data/AppDbContext.cs
@@ -1,2 +1,17 @@
-public DbSet<User> Users { get; set; }
-public 
+using Microsoft.EntityFrameworkCore;
+using ShipCore.Models;
+
+namespace ShipCore.Data;
+
+ // Creates your custom DB client by extending EF Core’s built-in DbContext.
+
+public class AppDbContext : DbContext
+{
+    // this below is simply contructor. if .NET will give our DbContext configuration such as:
+    public AppDbContext(
+         DbContextOptions<AppDbContext> options
+     ) : base(options)
+    {
+    }
+
+}
